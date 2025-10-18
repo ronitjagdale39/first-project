fcca728d961745854290a98b7b0a1f28b22a9bd8
Ronit Jagdale <ronit.jagdale.39@gmail.com>
2025-10-18 23:25:47 +0530

    added telecom.py

---
+++ telecom.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df=pd.read_csv('/Users/rony/Downloads/python modules/python projects/JAVA DSA/github project/telecom_churn predictor/telecom_churn_data.csv')
+df1=pd.DataFrame(df)
+print(df1)
+df1.info()
+
